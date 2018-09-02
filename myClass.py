a211d7ced0637480b9adc2fe61ace1d365eecaa0
xcollantes <xcollantes@users.noreply.github.com>
2018-09-01 19:29:59 -0700

    Abandoning py2exe

--- myClass.py
+++
@@ -1,8 +0,0 @@
-import datetime, pyperclip
-
-def myFunc():
-    print ("This is the test script ready for PROD.")
-    print (datetime.datetime.now())
-    print ("\n")
-    print ("Contents of clipboard: ")
-    print (pyperclip.paste())
