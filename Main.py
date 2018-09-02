d6e7e4c200028717f9966c1857ad5f197919134c
xcollantes <xcollantes@users.noreply.github.com>
2018-09-01 17:13:47 -0700

    Added libs and main

--- Main.py
+++ Main.py
@@ -3,5 +3,9 @@
 # purpose: Taking py2exe for a spin for Windows ready executable code.
 # Dependancies will hopefully be intact.
 
-import os, sys
+import os, sys, datetime
 
+if __name__=='__main__':
+    print ("This is the test script ready for PROD.")
+    print (datetime.datetime.now())
+    print ("\n")
