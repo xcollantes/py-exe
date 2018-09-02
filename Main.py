164ea8523ba6507f19dec59fff6fdaf7f9051473
xcollantes <xcollantes@users.noreply.github.com>
2018-09-01 19:33:58 -0700

    Install pyinstaller

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 # author: Xavier Collantes
 # date: 09/01/2018
-# purpose: Taking py2exe for a spin for Windows ready executable code.
+# purpose: Taking pyinstaller for a spin for Windows ready executable code.
 # Dependancies will hopefully be intact.
 
-from distutils.core import setup
-import py2exe
-setup(console=['myClass.py'])
+
